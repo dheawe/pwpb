6a603da45d451fa68c8aa4080e9598877f8cce3b
dheawe <97649378+dheawe@users.noreply.github.com>
2022-03-25 15:02:04 +0700

    Add files via upload

---
+++ pengayaan dhee/admin/init.php
@@ -0,0 +1,9 @@
+<?php 
+//menghubungkan ke App.php yang mengatur URL
+require_once 'core/App.php';
+
+//menghubungkan ke Controller utama
+require_once 'core/Controller.php';
+
+//menghubungkan ke file konfigurasi
+require_once 'config/Config.php';
