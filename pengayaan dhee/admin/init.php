a69ecae7f6252705edd7e6524ec2cea67a51fa20
dheawe <97649378+dheawe@users.noreply.github.com>
2022-04-06 11:29:23 +0700

    Delete pengayaan dhee directory

--- pengayaan dhee/admin/init.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//menghubungkan ke App.php yang mengatur URL
-require_once 'core/App.php';
-
-//menghubungkan ke Controller utama
-require_once 'core/Controller.php';
-
-//menghubungkan ke file konfigurasi
-require_once 'config/Config.php';
